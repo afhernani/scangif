b6478d81169e5046834862e3e206a41d0f5e5472
hernani Aleman Ferraz <afhernani@hotmail.com>
2019-02-01 01:25:45 +0000

    double click imagenBox lunch video, viewer

--- ScanGifDir/Viewer.cs
+++ ScanGifDir/Viewer.cs
@@ -12,6 +12,8 @@ using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using LibPanes;
+using System.IO;
+using System.Diagnostics;
 
 namespace ScanGifDir
 {
@@ -69,6 +71,8 @@ namespace ScanGifDir
 						Width=220,
 						Height=110,
 					};
+                    pn.DoubleClick += pn_DoubleClick;
+                    pn.Tag = _listaficheros[Index];
 					pn.FromFile(_listaficheros[Index]);
                     pn.SetCurrentFrame = (int)Math.Floor((decimal)(pn.Count / 2));
 					if (Index < _listaficheros.Count-1)
@@ -81,7 +85,17 @@ namespace ScanGifDir
 				ContadorActual = 0;
 			}
 		}
-		void AddSprite(ImagenBox pn)
+
+        private void pn_DoubleClick(object sender, EventArgs e)
+        {
+            ImagenBox pn = (ImagenBox)sender;
+            string CurrentFilePath = pn.Tag.ToString();
+            if (String.IsNullOrEmpty(CurrentFilePath)) return;
+            string movie = CombineAddressFileMovie(CurrentFilePath);
+            if (File.Exists(movie)) Process.Start(movie);
+        }
+
+        void AddSprite(ImagenBox pn)
 		{
 			if(flowLayoutPanel.Controls.Count>LimtConatrols-1)
 				flowLayoutPanel.Controls.RemoveAt(0);
@@ -92,5 +106,51 @@ namespace ScanGifDir
 			//flowLayoutPanel.Controls.Clear();
 			t = Task.Factory.StartNew(AddSpriteToControlFlowLayoutPane);
 		}
-	}
+        /// <summary>
+        /// encontrar el nombre del fichero de video referenciado.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private static string GetFileNameFromString(string name)
+        {
+            int pos = name.IndexOf(@"_thumbs_", StringComparison.Ordinal);
+            if (pos == -1)
+                return name;
+            name = name.Substring(0, pos);
+            return Path.GetFileName(name);
+        }
+
+        private static string CombineAddressFileMovie(string currentfilepath)
+        {
+            string name = null;
+            string cad = null;
+            if (!String.IsNullOrEmpty(currentfilepath))
+            {
+                name = GetFileNameFromString(currentfilepath);
+                cad = Path.Combine(PathNivel(Path.GetDirectoryName(currentfilepath), 1), name);
+            }
+
+            Debug.WriteLine($"direccion fichero: {cad}");
+            return cad;
+        }
+        /// <summary>
+		/// Utilidad para retroceder
+		/// devuelve el nivel n veces inferior en el path
+		/// </summary>
+		/// <param name="root"></param>
+		/// <param name="n"></param>
+		/// <returns></returns>
+	    private static string PathNivel(string root, int n)
+        {
+            string[] name = root.Split(Convert.ToChar(@"\"));
+            string path = name[0];
+            for (int i = 1; i < name.Length - n; i++)
+            {
+                path += @"\" + name[i];
+            }
+
+            return path;
+        }
+
+    }
 }
